d7f5775a9fcec4643a11666a61876db8085c7d88
xDerrick23 <dinodidiodoro23@gmail.com>
2024-01-26 19:52:03 +0100

    lectures

--- Main.java
+++ Main.java
@@ -1,12 +1,10 @@
 import entities.Lectures;
 
-import java.util.Date;
-
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Lectures lettura = new Lectures(16, "paperino", new Date());
-
+        Lectures lettura = new Lectures(16, "paperino", 2024,300);
+        System.out.println(lettura.toString());
     }
 }
